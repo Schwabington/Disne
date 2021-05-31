dd6d3498528aa1f0a4bd55f15cc0b0169983ee02
Schwabington <73295805+Schwabington@users.noreply.github.com>
2021-05-31 14:58:18 +0100

    Delete java directory

--- java/src/main/java/com/disneystreaming/i21/JsonDecodeException.java
+++
@@ -1,9 +0,0 @@
-package com.disneystreaming.i21;
-
-public class JsonDecodeException extends RuntimeException {
-
-    public JsonDecodeException(Throwable cause) {
-        super(cause);
-    }
-
-}
